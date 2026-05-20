a1c0c0b349e4d05ffebf8497612579919e75577a
Aymen21-n <aymen.nafis@emsi-edu.ma>
2026-05-20 17:31:23 +0100

    Add folder with report and presentation and video

--- app/src/main/java/com/example/quizapp_nafis/Constants.java
+++ app/src/main/java/com/example/quizapp_nafis/Constants.java
@@ -1,5 +1,5 @@
 package com.example.quizapp_nafis;
 
 public class Constants {
-    public static final String FASTAPI_BASE_URL = "https://picture-nokia-instantly-saving.trycloudflare.com";
+    public static final String FASTAPI_BASE_URL = " https://quality-products-exceptions-guidelines.trycloudflare.com";
 }
